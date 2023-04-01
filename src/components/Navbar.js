e4eb8c4d50afcd243d130d6e5781837db8abe845
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-04-01 18:02:09 +0300

    basics

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'> 
+        <ul className='navbar-list'>
+            <li className='glance-title'>GLANCE</li>
+            <li>Events</li>
+            <li>About Us</li>
+            <li>Contact</li>
+            <li>Our Team</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
