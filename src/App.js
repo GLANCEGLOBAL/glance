0df76d1807deed4985e835e5bc2dab6c9e6bf7b8
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-04-19 21:18:44 +0300

    react-router eklendi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Events from "./pages/Events";
 function App() {
   return (
-    <div className="App">
-     <Navbar />
-     <button className='join-us'>Join Us</button>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <button className="join-us">Join Us</button>
+        <Routes>
+          <Route path="/events" element={<Events />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
