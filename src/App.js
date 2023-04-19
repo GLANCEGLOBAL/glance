fa74a91f85c61d557ead569d4cbebfd06f81a86b
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-04-19 21:20:43 +0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Events from "./pages/Events";
+import AboutUs from "./pages/AboutUs";
+import Contact from "./pages/Contact";
+import OurTeam from "./pages/OurTeam";
 function App() {
   return (
     <Router>
@@ -13,7 +12,10 @@ function App() {
         <Navbar />
         <button className="join-us">Join Us</button>
         <Routes>
-          <Route path="/events" element={<Events />}/>
+          <Route path="/events" element={<Events />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/ourteam" element={<OurTeam />} />
         </Routes>
       </div>
     </Router>
