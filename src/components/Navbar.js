991f4e1939dacc5ab24de1ea0964fec2ddc8f0b4
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-04-19 21:45:37 +0300

    ikon dosyası oluşturuldu ve görsel yüklendi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,10 @@ const Navbar = () => {
     <div className="navbar">
       <ul className="navbar-list">
         <li className="glance-title">GLANCE</li>
-        <li><Link to="/events">Events</Link></li>
-        <li><Link to="/aboutus">About Us</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/ourteam">Our Team</Link></li>
+        <li><Link className="router-link" to="/events">Events</Link></li>
+        <li><Link className="router-link" to="/aboutus">About Us</Link></li>
+        <li><Link className="router-link" to="/contact">Contact</Link></li>
+        <li><Link className="router-link" to="/ourteam">Our Team</Link></li>
       </ul>
     </div>
   );
