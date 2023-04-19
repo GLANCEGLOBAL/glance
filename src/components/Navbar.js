a5aa2d4dd65afa812334cd3422e1c0bbb05c56f8
Efe Özen <111181897+efezen@users.noreply.github.com>
2023-04-19 21:31:28 +0300

    sayfa yolları react router ile navbara eklendi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <ul className="navbar-list">
         <li className="glance-title">GLANCE</li>
-        <li>Events</li>
-        <li>About Us</li>
-        <li>Contact</li>
-        <li>Our Team</li>
+        <li><Link to="/events">Events</Link></li>
+        <li><Link to="/aboutus">About Us</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/ourteam">Our Team</Link></li>
       </ul>
     </div>
   );
